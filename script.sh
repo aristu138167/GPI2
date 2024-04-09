c2d8565027cb5dc486be913610c7395047eeeaa7
Urki Aristu Viela <aristu.138167@e.unavarra.es>
2024-04-09 18:55:07 +0200

    Initial commit

---
+++ script.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/sakaiproject/sakai.git
+cd sakai
+sed -i '/<id>all<\/id>/,/<id>experimental<\/id>/ { /<module>/ { /<module>kernel<\/module>/!d }; }' pom.xml
+sed -i '/<id>experimental<\/id>/,/<id>pack<\/id>/ { /<module>/ { /<module>kernel<\/module>/!d }; }' pom.xml
+mvn install
