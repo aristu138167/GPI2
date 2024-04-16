e3f5c1b4cbc23fbec86fd46ae7a74952af324071
Urki Aristu Viela <aristu.138167@e.unavarra.es>
2024-04-16 19:50:06 +0200

    Entregables y pmd

--- script.sh
+++ script.sh
@@ -3,3 +3,5 @@ cd sakai
 sed -i '/<id>all<\/id>/,/<id>experimental<\/id>/ { /<module>/ { /<module>kernel<\/module>/!d }; }' pom.xml
 sed -i '/<id>experimental<\/id>/,/<id>pack<\/id>/ { /<module>/ { /<module>kernel<\/module>/!d }; }' pom.xml
 mvn install
+mvn package
+pmd check -d kernel -R rulesets/java/quickstart.xml -f text
